d3fdc7f4efd7fba1419b81f1fd6470554f295a89
Ouday Ayoubi <ouday.ayoubi@inmobiles.net>
2021-11-04 10:21:24 +0200

    Initial project setup

---
+++ OudaySDK/OudaySDK.h
@@ -0,0 +1,18 @@
+//
+//  OudaySDK.h
+//  OudaySDK
+//
+//  Created by InMobiles on 11/4/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OudaySDK.
+FOUNDATION_EXPORT double OudaySDKVersionNumber;
+
+//! Project version string for OudaySDK.
+FOUNDATION_EXPORT const unsigned char OudaySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OudaySDK/PublicHeader.h>
+
+
---
+++ OudaySDKExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OudaySDKExamples
+//
+//  Created by InMobiles on 11/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
